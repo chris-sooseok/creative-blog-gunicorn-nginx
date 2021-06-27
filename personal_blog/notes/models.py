83f4fdcb27cf8dd4114f648cf1c5b7a206bd4b99
chris_sosoeok <sooseokkim99@gmail.com>
2021-06-27 15:08:42 +0900

    title note max length

--- personal_blog/notes/models.py
+++ personal_blog/notes/models.py
@@ -16,7 +16,7 @@ class Topic(models.Model):
 class Note(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE,related_name="notes",)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=50)
     summary = models.CharField(max_length=200, blank=True)
     date = models.DateField(auto_now=True)
     content = RichTextUploadingField(blank=True, null=True)
