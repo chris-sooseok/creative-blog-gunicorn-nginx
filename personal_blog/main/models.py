b32263c65cd9b70f2d94016161069580e8db1d8e
chris_sosoeok <sooseokkim99@gmail.com>
2021-07-10 13:06:13 +0900

    weather debug

--- personal_blog/main/models.py
+++ personal_blog/main/models.py
@@ -7,9 +7,9 @@ class City(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=40, blank=True)
-    temperature = models.CharField(max_length=20, blank=True)
-    description = models.CharField(max_length=20, blank=True)
-    icon = models.CharField(max_length=20, blank=True)
+    temperature = models.CharField(max_length=40, blank=True)
+    description = models.CharField(max_length=40, blank=True)
+    icon = models.CharField(max_length=40, blank=True)
     # check if url is valid with the city name
 
     def __str__(self):
